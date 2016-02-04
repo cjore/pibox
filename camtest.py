9446028aa6323a10d15864833f59ea46aa2433fc
cjore <cjore@gmail.com>
2016-02-04 12:49:37 +0000

    ajout de feature
    description détaillée

--- camtest.py
+++ camtest.py
@@ -4,6 +4,9 @@ import picamera
 from time import sleep
 
 camera = picamera.PiCamera()
-camera.start_preview()
-sleep(10)
+#camera.resolution = (1280,720)
+camera.start_preview(fullscreen=False, window=(100,20,1280,960))
+camera.annotate_background = picamera.Color('black')
+camera.annotate_text='Hello World'
+sleep(5)
 camera.stop_preview()
