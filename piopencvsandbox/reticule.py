0bd358e892a70f34d0850e653795593b388ef820
cjore <chris.jore@gmail.com>
2016-05-19 15:33:33 +0200

    mise en place du déplacement de la réticule

--- piopencvsandbox/reticule.py
+++ piopencvsandbox/reticule.py
@@ -7,6 +7,35 @@ import cv2
 import time
 import imutils
 
+mx = 50
+my = 50
+mxcourant = mx
+mycourant = my
+
+# mouse callback function
+def moveTarget(event,x,y,flags,param):
+	if event == cv2.EVENT_MOUSEMOVE :
+		global mx
+		global my
+		#mx = int(x/10)
+		#my = int(y/10)
+		
+		if (x - 50 < 0):
+			mx = 0 
+		elif (x + 100 > 1280):
+			mx = 1280 -100
+		else:
+			mx = x - 50
+	
+		
+		if (y - 50 < 0): 
+			my = 0
+		elif (y + 100 > 720):	
+			my = 720 - 100
+		else:
+			my = y - 50	
+		print('x : ' + str(x) + " - " + " y : " + str(y))
+		
 # created a "threaded" video stream, allow the camera sensor to warmup
 vs = PiVideoStream((1280, 720)).start()
 time.sleep(1.0)
@@ -16,10 +45,12 @@ reticule = cv2.imread("target-red.png")
 reticule = imutils.resize(reticule, width=100)
 rows, cols, channels = reticule.shape
 
+
 # Masks creation
 grayreticule = cv2.cvtColor(reticule, cv2.COLOR_BGR2GRAY)
 ret, mask = cv2.threshold(grayreticule, 127, 255, cv2.THRESH_BINARY)
 mask_inv = cv2.bitwise_not(mask)
+
 
 # Loop over the frames of the video
 while True :
@@ -28,17 +59,12 @@ while True :
 	frame = vs.read()
 	
 	frame2 = imutils.resize(frame, width=1280)
-	#reticule = imutils.resize(reticule, width=50)
-	
-	#grayreticule = cv2.cvtColor(reticule, cv2.COLOR_BGR2GRAY)
-	#grayframe = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-	
-	#x_offset=y_offset=50
-	#grayframe[y_offset:y_offset+reticule.shape[0], x_offset:x_offset+reticule.shape[1]] = grayreticule
-	
-	# ROI definition
-	roi = frame2[0:rows, 0:cols]
 
+	# ROI definition
+	roi = frame2[my:rows+my, mx:cols+mx]
+	
+	print('mx : ' + str(mx) + " - my : " + str(my) + " - rows : " +  str(rows+mx) + " - cols : " + str(cols+my))
+	
 	# black-out the area of reticule in ROI
 	frame_bg = cv2.bitwise_and(roi, roi, mask = mask)
 	#print(frame_bg.shape)
@@ -49,10 +75,13 @@ while True :
 	
 	# put logo in ROI and modify the main frame
 	dst = cv2.add(frame_bg, reticule_fg)
-	frame2[0:rows, 0:cols] = dst
+	frame2[my:rows+my, mx:cols+mx] = dst
 		
 	# show the frame 
 	cv2.imshow("Target", frame2)
+	
+	
+	cv2.setMouseCallback("Target", moveTarget)
 		
 	# wait user action
 	key = cv2.waitKey(1) & 0xFF
