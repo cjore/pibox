a8327e82269bbacf2cefb3fdb3a4b6f10d374763
cjore <chris.jore@gmail.com>
2016-09-01 18:49:51 +0200

    premier essai de tts

---
+++ pittssandbox/piespeak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from espeak import espeak
+#import espeak
+import subprocess
+
+text = '"Safe mode!"'
+subprocess.call('espeak -v fr+m6 '+text, shell=True)
+subprocess.call('espeak '+text, shell=True)
+
+espeak.list_voices()
+espeak.set_voice('fr')
+espeak.synth("Hello Instructables!")
+
+#def voices = espeak.list_voices()
+#for voice in voices :
+#	print voice
